7bdabcd56728782905baafbc47d835d42b04125f
Dominik Szmek <do@minik.pl>
2018-11-08 19:46:58 +0100

    add psql setting for prod

--- domino/settings/prod.py
+++ domino/settings/prod.py
@@ -2,4 +2,14 @@ from .base import * # noqa
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': get_secret('DB_ENGINE'),
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('DB_USER'),
+        'HOST': get_secret('DB_HOST'),
+        'PORT': get_secret('DB_PORT'),
+    }
+}
+
 ALLOWED_HOSTS += ['10.4.0.1', 'domino.d']
