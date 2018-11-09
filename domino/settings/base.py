21564168aee1f80498ba1bd95c0c82a7c136275d
Dominik Szmek <do@minik.pl>
2018-11-09 08:46:24 +0100

    add STATIC_ROOT

--- domino/settings/base.py
+++ domino/settings/base.py
@@ -89,17 +89,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'domino.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -138,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # add STATIC_ROOT to DIRS
