51c84f3e2b9174b8eb5599e533c6d3cf319cebb0
Dominik Szmek <do@minik.pl>
2018-11-28 08:36:22 +0100

    start transfers app

--- domino/urls.py
+++ domino/urls.py
@@ -22,6 +22,7 @@ import os
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),
+    path('transfers/', include('transfers.urls')),
     # path('accounts/', include('django.contrib.auth.urls'))
 ]
 
