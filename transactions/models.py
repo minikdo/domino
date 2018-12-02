b811db83b855cd62f1dee14f2413c2980a835675
Dominik Szmek <do@minik.pl>
2018-12-02 14:26:13 +0100

    add transaction model and migration

--- transactions/models.py
+++ transactions/models.py
@@ -1,5 +1,100 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Txransfer(models.Model):
+class TransactionStatus(models.Model):
+    """ transaction status """
+
+    name = models.CharField(max_length=30, verbose_name="status")
+
+    class Meta:
+        verbose_name = "status"
+        verbose_name_plural = "statusy"
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    """ transactions """
+    """ format EXILIR-0 """
+    
+    transaction_type = models.IntegerField()
+    execution_date = models.IntegerField()
+    amount = models.IntegerField()
+    ordering_bank = models.IntegerField()
+    ordering_account = models.CharField(max_length=34)
+    counterparty_account = models.CharField(max_length=34)
+    ordering_name_address = models.CharField(max_length=150)
+    counterparty_name_address = models.CharField(max_length=150)
+    counterparty_bank = models.IntegerField()
+    order_title = models.CharField(max_length=150)
+    transaction_classification = models.IntegerField()
+    annotations = models.CharField(max_length=32)
+    # additional fields:
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    status = models.ForeignKey('TransactionStatus',
+                               null=True,
+                               on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = "transakcja"
+        verbose_name_plural = "transakcje"
+
+    def __str__(self):
+        string = "{}, {}, {}".format(
+            self.execution_date,
+            self.counterparty_account,
+            self.amount)
+        return string
+
+
+class TransactionFile(models.Model):
+    """ transaction package """
+
     pass
+
+
+class Counterparty(models.Model):
+    """ counterparties """
+
+    account = models.ForeignKey('Account',
+                                on_delete=models.SET_NULL,
+                                null=True,
+                                verbose_name="konto")
+    name = models.CharField(max_length=35, null=True, verbose_name="nazwa")
+    street = models.CharField(max_length=35, null=True, verbose_name="ulica")
+    city = models.CharField(max_length=35, null=True, verbose_name="miasto")
+    tax_id = models.CharField(max_length=35, null=True, verbose_name="NIP")
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "kontrahent"
+        verbose_name_plural = "kontrahenci"
+
+    def __str__(self):
+        string = "{}, {}, {}, {}".format(
+            self.name,
+            self.street,
+            self.city,
+            self.nip)
+        return string
+
+
+class Account(models.Model):
+    """ account numbers """
+    """ one counterparty may have many bank accounts """
+
+    account = models.CharField(max_length=34, verbose_name="numer konta")
+    comment = models.CharField(max_length=50, verbose_name="komentarz")
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "konto bankowe"
+        verbose_name_plural = "konta bankowe"
+
+    def __str__(self):
+        return self.account
--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,12 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Transaction
 
 
-class IndexView(TemplateView):
-    pass
+class IndexView(LoginRequiredMixin, TemplateView):
+    """ transaction list """
+
+    model = Transaction
+    template_name = 'transactions/index.html'
