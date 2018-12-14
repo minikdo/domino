0f0b48c9136c19a4c90eb7d980cb2fe59b774902
Dominik Szmek <do@minik.pl>
2018-12-14 09:23:10 +0100

    register transactions to admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import OrderingAccount, TransactionStatus
+
+admin.site.register(OrderingAccount)
+admin.site.register(TransactionStatus)
