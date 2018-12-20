8385b9d26b8b071bbebb783455ab1c799b3677a8
Dominik Szmek <do@minik.pl>
2018-12-20 13:47:45 +0100

    register models to admin

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Invoice, InvoiceItem, Customer
+
+
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
+admin.site.register(Customer)
