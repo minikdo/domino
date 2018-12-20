d2e78788e6d7b426cafd5f6752ec71a84a6b2111
Dominik Szmek <do@minik.pl>
2018-12-20 19:22:29 +0100

    add new views and fix models

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,114 @@
 from django.db import models
+from django.urls import reverse
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from transactions.validators import validate_tax_id
+from transactions.models import TimeStampedModel
+from inventory.models import Make
+
+
+class Invoice(TimeStampedModel):
+    """ invoices """
+
+    number = models.CharField(max_length=35, verbose_name="numer faktury")
+    customer = models.ForeignKey('Customer',
+                                 verbose_name="klient",
+                                 on_delete=models.CASCADE)
+    issued = models.DateField(verbose_name="data wystawienia")
+    transaction = models.DateField(verbose_name="data sprzedaży")
+    due = models.DateField(verbose_name="termin płatności")
+    
+    def __str__(self):
+        string = "{}, {}, {}".format(
+            self.number, self.issued, self.customer)
+        return string
+
+    def get_absolute_url(self):
+        return reverse('invoices:detail',
+                       kwargs={'pk': self.id})
+
+
+class InvoiceItem(models.Model):
+    """ items on invoice """
+
+    invoice = models.ForeignKey('Invoice', on_delete=models.CASCADE)
+    make = models.ForeignKey(Make, verbose_name="towar",
+                             on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2,
+                                verbose_name="cena brutto")
+    vat = models.FloatField(default=23, help_text="%")
+    qty = models.FloatField(default=1, verbose_name="ilość")
+
+    @property
+    def net_price(self):
+        """ calculate net price """
+        
+        return round(float(self.price)/(1+(self.vat/100)), 2)
+
+    def __str__(self):
+        string = "{}, {}, {}, {}".format(
+            self.make, self.qty, self.price, self.vat)
+        return string
+
+    def get_absolute_url(self):
+        return reverse('invoices:detail',
+                       kwargs={'pk': self.invoice.pk})
+
+
+class Customer(TimeStampedModel):
+    """ counterparties """
+
+    company = models.CharField(max_length=35,
+                               null=True,
+                               blank=True,
+                               verbose_name="nazwa firmy")
+    name = models.CharField(max_length=35,
+                            null=True,
+                            blank=True,
+                            verbose_name="imię i nazwisko")
+    street = models.CharField(max_length=35,
+                              null=True,
+                              blank=True,
+                              verbose_name="ulica")
+    city = models.CharField(max_length=35,
+                            blank=True,
+                            null=True,
+                            verbose_name="miasto")
+    tax_id = models.CharField(max_length=35,
+                              blank=True,
+                              null=True,
+                              unique=True,
+                              verbose_name="NIP",
+                              help_text="tylko cyfry")
+    email = models.EmailField(max_length=50,
+                              blank=True,
+                              null=True)
+    phone = models.CharField(max_length=35,
+                             blank=True,
+                             null=True,
+                             verbose_name="telefon")
+
+    class Meta:
+        verbose_name = "klient"
+        verbose_name_plural = "klienci"
+
+    def __str__(self):
+        string = "{}, {}, {}, {}, {}".format(
+            self.company,
+            self.name,
+            self.street,
+            self.city,
+            self.tax_id)
+        return string
+
+    def get_absolute_url(self):
+        return reverse('invoices:customer-detail',
+                       kwargs={'pk': self.id})
+
+    def clean(self):
+        if self.tax_id is not None and\
+           self.tax_id.isdigit() and\
+           not validate_tax_id(self.tax_id):
+            raise ValidationError({'tax_id':
+                                   _('Nieprawidłowa suma kontrolna NIP')})
--- invoices/views.py
+++ invoices/views.py
@@ -1,3 +1,151 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
-# Create your views here.
+from djatex import render_latex
+
+from .models import Invoice, InvoiceItem, Customer
+from .forms import InvoiceForm, InvoiceItemForm
+from .utils import SumItems
+from transactions.mixins import CreatedByMixin
+
+
+@login_required
+def latex(request, **kwargs):
+    """Generate an invoice in PDF"""
+    
+    invoice = kwargs['invoice']
+
+    invoice_data = Invoice.objects.get(pk=invoice)
+    items = InvoiceItem.objects.filter(invoice=invoice)
+           
+    # inventory = Inventory.objects.get(pk=inventory_id)
+
+    file_name = "faktura_{id}_z_{date}.pdf".format(
+        id=1,
+        date=2)
+    
+    context = {'invoice': invoice_data,
+               'items': items,
+               'total': SumItems(invoice)}
+    
+    return render_latex(request, file_name, 'invoices/invoice.tex',
+                        error_template_name='invoices/error.html',
+                        home_dir=settings.TEX_HOME,
+                        context=context)
+
+
+class IndexView(LoginRequiredMixin, ListView):
+    """List of invoices"""
+
+    model = Invoice
+    template_name = 'invoices/index.html'
+
+
+class InvoiceDetailView(LoginRequiredMixin, DetailView):
+    """Invoice detail"""
+
+    model = Invoice
+    template_name = 'invoices/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        invoice = self.kwargs['pk']
+        items = InvoiceItem.objects.filter(invoice=invoice)
+
+        context['items'] = items
+        context['total'] = SumItems(invoice)
+        
+        return context
+    
+
+class InvoiceCreateView(LoginRequiredMixin, CreatedByMixin, CreateView):
+    """Create an invoice"""
+
+    model = Invoice
+    template_name = 'invoices/create.html'
+    form_class = InvoiceForm
+
+    def get_initial(self):
+        return {'customer': self.kwargs['customer']}
+    
+
+class InvoiceUpdateView(LoginRequiredMixin, CreatedByMixin, UpdateView):
+    """Update an invoice"""
+
+    model = Invoice
+    template_name = 'invoices/create.html'
+    form_class = InvoiceForm
+    
+
+class InvoiceItemCreateView(LoginRequiredMixin, CreateView):
+    """Create an invoice item"""
+
+    model = InvoiceItem
+    template_name = 'invoices/invoice_item_create.html'
+    form_class = InvoiceItemForm
+    
+    def get_initial(self):
+        return {'invoice': self.kwargs['invoice']}
+
+
+class InvoiceItemUpdateView(LoginRequiredMixin, UpdateView):
+    """Update an invoice item"""
+
+    model = InvoiceItem
+    template_name = 'invoices/invoice_item_create.html'
+    form_class = InvoiceItemForm
+
+
+class InvoiceItemDeleteView(LoginRequiredMixin, DeleteView):
+    """Delete an invoice item"""
+
+    model = InvoiceItem
+    template_name = 'invoices/_confirm_delete.html'
+
+    def get_success_url(self):
+        invoice = self.object.invoice
+        return reverse_lazy('invoices:detail', kwargs={'pk': invoice.pk})
+
+
+class CustomerIndexView(LoginRequiredMixin, ListView):
+    """List of customers"""
+
+    model = Customer
+    template_name = 'invoices/customer_index.html'
+
+
+class CustomerDetailView(LoginRequiredMixin, DetailView):
+    """Customer details"""
+    
+    model = Customer
+    template_name = 'invoices/customer_detail.html'
+
+    
+class CustomerCreateView(LoginRequiredMixin, CreatedByMixin, CreateView):
+    """Create a customer"""
+
+    model = Customer
+    template_name = 'invoices/customer_create.html'
+    fields = ['company', 'name', 'street', 'city', 'tax_id', 'email', 'phone']
+    
+
+class CustomerUpdateView(LoginRequiredMixin, UpdateView):
+    """Update a customer"""
+
+    model = Customer
+    template_name = 'invoices/customer_update.html'
+    fields = ['company', 'name', 'street', 'city', 'tax_id', 'email', 'phone']
+
+
+class CustomerDeleteView(LoginRequiredMixin, DeleteView):
+    """Delete a customer"""
+
+    model = Customer
+    template_name = 'invoices/_confirm_delete.html'
