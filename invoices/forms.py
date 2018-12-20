53d14baab7fd26a123b4a2b8c6f2e5963886dd2c
Dominik Szmek <do@minik.pl>
2018-12-20 19:19:38 +0100

    add invoice item form

--- invoices/forms.py
+++ invoices/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
-from .models import Invoice
+from datetime import date
+
+from .models import Invoice, InvoiceItem
+from inventory.models import Make
 
 
 class InvoiceForm(forms.ModelForm):
@@ -9,7 +12,31 @@ class InvoiceForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['customer'].disabled = True
 
+    transaction = forms.DateField(initial=date.today(),
+                                  label="Data sprzedaży",
+                                  widget=forms.DateInput(format='%Y-%m-%d'),
+                                  input_formats=['%Y-%m-%d'])
+
+    issued = forms.DateField(initial=date.today(),
+                             label="Data wystawienia",
+                             widget=forms.DateInput(format='%Y-%m-%d'),
+                             input_formats=['%Y-%m-%d'])
+
+    due = forms.DateField(initial=date.today(),
+                          label="Termin płatności",
+                          widget=forms.DateInput(format='%Y-%m-%d'),
+                          input_formats=['%Y-%m-%d'])
+
     class Meta:
         model = Invoice
         fields = ['number', 'issued', 'customer', 'transaction',
-                  'due']        
+                  'due']
+
+
+class InvoiceItemForm(forms.ModelForm):
+
+    make = forms.ModelChoiceField(Make.objects.all())
+    
+    class Meta:
+        model = InvoiceItem
+        fields = '__all__'
