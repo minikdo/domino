025ba7b1cf1e1e4c980e439fbbd41df4ceb84c21
Dominik Szmek <do@minik.pl>
2019-08-25 18:52:53 +0200

    change field label

--- gold/forms.py
+++ gold/forms.py
@@ -9,11 +9,11 @@ class ContractForm(forms.Form):
                            label="Data sprzedaży",
                            widget=forms.DateInput(format='%Y-%m-%d'),
                            input_formats=['%Y-%m-%d'])
-    
+
     number = forms.CharField(label="Numer umowy",
                              widget=forms.TextInput(
                                  attrs={'autofocus': True}))
 
-    seller = forms.CharField(label="Sprzedający")
+    seller = forms.CharField(label="Umowa zawarta z")
     weight = forms.FloatField(label="Waga")
     price = forms.FloatField(label="Cena")
