a0e8eb5ea104c64cd2139f2491f0d5e859b477a0
Dominik Szmek <do@minik.pl>
2019-08-26 15:43:17 +0200

    add validation

--- gold/forms.py
+++ gold/forms.py
@@ -1,14 +1,17 @@
 from django import forms
+from django.contrib.admin import widgets
+from django.core.exceptions import ValidationError
+# from django.utils.translation import gettext_lazy as _
 
 from datetime import date
 
 
 class ContractForm(forms.Form):
 
-    date = forms.DateField(initial=date.today(),
-                           label="Data sprzedaży",
-                           widget=forms.DateInput(format='%Y-%m-%d'),
-                           input_formats=['%Y-%m-%d'])
+    date = forms.DateField(widget=widgets.AdminDateWidget(),
+                           localize=False,
+                           initial=date.today(),
+                           input_formats=['%d.%m.%Y'])
 
     number = forms.CharField(label="Numer umowy",
                              widget=forms.TextInput(
@@ -17,3 +20,14 @@ class ContractForm(forms.Form):
     seller = forms.CharField(label="Umowa zawarta z")
     weight = forms.FloatField(label="Waga")
     price = forms.FloatField(label="Cena")
+
+    class Media:
+        js = ('admin/js/vendor/jquery/jquery.js',
+              'admin/js/core.js')
+        css = {'all': ('admin/css/forms.css',)}
+
+    def clean_seller(self):
+        seller = self.cleaned_data['seller']
+        if len(seller) > 50:
+            raise ValidationError("Nazwa przekracza 50 znaków")
+        return seller
