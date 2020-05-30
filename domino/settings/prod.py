01dad3ce57bae7135ad36316a08d24b0aa72a342
Dominik Szmek <do@minik.pl>
2020-05-30 11:32:08 +0200

    add loggers

--- domino/settings/prod.py
+++ domino/settings/prod.py
@@ -5,3 +5,19 @@ DEBUG = False
 ALLOWED_HOSTS += ['10.4.0.1', 'domino.d']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGGING = {
+    'loggers': {
+        'django': {
+            handlers: {['file']},
+            level: 'DEBUG'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './django.log'
+        }
+    }
+}
