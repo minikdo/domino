36f920ef14c4d622551ba57d152577799417e56c
Dominik Szmek <do@minik.pl>
2020-05-31 18:14:59 +0200

    move ALLOWED_HOSTS to secrets.json

--- domino/settings/prod.py
+++ domino/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *  # noqa
 
 DEBUG = False
 
-ALLOWED_HOSTS += ['10.4.0.1', 'domino.d']
+ALLOWED_HOSTS += get_secret('ALLOWED_HOSTS')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
