6ab6340710dd7f98084c6ade508e436421e4bf8c
Dominik Szmek <do@minik.pl>
2022-06-13 10:54:03 +0200

    Fix bug #1: expression contains mixed types

--- inventory/utils.py
+++ inventory/utils.py
@@ -1,19 +1,20 @@
-from django.db.models import Sum, Q, Count, Value, F
+from django.db.models import Sum, Q, Count, Value, F, IntegerField
 from django.db.models.functions import Coalesce
 from .models import Item, Unit, Inventory
 
 
 def shelf_counter(inventory, created_by, shelf_id):
-    
+
     if any(var is None for var in [inventory, created_by, shelf_id]):
         return 0
-    
+
     item_num = Item.objects.filter(
         Q(inventory_id=inventory),
         Q(created_by=created_by),
         Q(pk__gte=shelf_id)).aggregate(
             piece=Coalesce(
-                Sum('quantity', filter=Q(unit_id=Unit.QTY_ID)), Value(0)),
+                Sum('quantity', filter=Q(unit_id=Unit.QTY_ID),
+                    output_field=IntegerField()), Value(0)),
             weight=Count('pk', filter=Q(unit_id=Unit.GRAM_ID)))
 
     return item_num
